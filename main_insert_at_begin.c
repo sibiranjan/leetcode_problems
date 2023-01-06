042f9af5948bcecb2c08d32184f9cc0512ace542
sibiranjan <122072801+sibiranjan@users.noreply.github.com>
2023-01-06 09:44:25 +0530

    Create main_insert_at_begin.c

---
+++ main_insert_at_begin.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+struct node{
+int data;
+struct node *next;
+};
+
+//creating a newnode
+struct node *Newnode(int data){
+  struct node *Newnode;
+  Newnode=(struct node*)malloc(sizeof(struct node));
+  Newnode->data=data;
+  Newnode->next=NULL;
+  return Newnode;
+}
+
+//Creating an insertion at end on linked list
+void insert_at_end(struct node **head,int data){
+  struct node *newnode = Newnode(data);
+  struct node *temp = *head;
+  if(temp==NULL){
+    *head=newnode;
+  }
+  else{
+    while(temp->next!=NULL){
+      temp=temp->next;
+    }
+    temp->next=newnode;
+  }
+  
+}
+int main(){
+  struct node *head=NULL;
+  insert_at_end(&head, 1);
+  insert_at_end(&head, 2);
+  insert_at_end(&head, 3);
+  insert_at_end(&head, 4);
+
+  printf("The linked list are");
+  struct node *temp;
+  temp=head;
+  while(temp!=NULL){
+    printf("%d->",temp->data);
+    temp=temp->next;
+  }
+  
+}
